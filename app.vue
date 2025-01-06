55cd0b5811c06f6fd0d2d270fca42f51eef918cb
Carl Jia <carl.jia@nxp.nz>
2025-01-06 11:48:34 +1100

    use correct icons

--- app.vue
+++ app.vue
@@ -6,4 +6,20 @@
 
 <script setup>
 import 'primeicons/primeicons.css'
+
+useHead({
+  title: 'Punchout Tester',
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { hid: 'description', name: 'description', content: 'Punchout Tester' }
+  ],
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+    { rel: 'manifest', href: '/site.webmanifest' }
+  ]
+})
 </script>
