08be326017b938e2c30f56ead899671fdbc8bc43
Carl Jia <carl.jia@nxp.nz>
2025-03-25 12:19:54 +1100

    Refactor cxml punchout

--- pages/cxml/index.vue
+++ pages/cxml/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-gray-100 p-4 flex gap-4">
     <Panel header="Punch Out Test - cXML">
-      <PunchoutForm/>
+      <CxmlPunchoutForm/>
     </Panel>
     <Panel header="Saved Profiles" class="flex-grow">
       <ProfileList type="cxml"/>
